097022ae376be68b5353c1c0786ccaa50252a75a
Volodymyr Kolesnykov <volodymyr@wildwolf.name>
2024-11-03 12:01:07 +0200

    fix: dependency name

--- cmake/wwa_jsonrpc-config.cmake
+++ cmake/wwa_jsonrpc-config.cmake
@@ -3,7 +3,7 @@ get_filename_component(JSONRPC_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 list(APPEND CMAKE_MODULE_PATH ${JSONRPC_CMAKE_DIR})
 
 include(CMakeFindDependencyMacro)
-find_dependency(nlohmann-json)
+find_dependency(nlohmann_json)
 
 if(NOT TARGET wwa_jsonrpc)
     include("${JSONRPC_CMAKE_DIR}/wwa_jsonrpc-target.cmake")
